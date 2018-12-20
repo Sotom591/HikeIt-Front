d415fcefb9f293fbbeeaf2e1a2a57dbb0993ffbc
github username <github email address>
2018-12-20 18:07:28 -0500

    attempting api key

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 
 import HomeContainer from './containers/HomeContainer'
 import NavBar from './components/NavBar'
+import TrailsContainer from './containers/TrailsContainer'
+
+// componentDidMount(){
+//   fetch('')
+// }
 
 class App extends Component {
   render() {
@@ -12,6 +17,7 @@ class App extends Component {
       <div className="App">
         < NavBar />
         <Route exact path='/' render={() => < HomeContainer /> } />
+        <Route exact path ='/trails' render={() => < TrailsContainer /> } />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+// //
+// const mykey = config.API_KEY
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
