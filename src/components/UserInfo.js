56a55d5e53f5b0849411f4d56d7f4ee5e834958f
github username <github email address>
2018-12-30 00:07:03 -0500

    editing user info page

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -7,9 +7,19 @@ export default class UserInfo extends Component {
 render(){
     return(
       <div>
-        {this.props.currentUser ? this.props.currentUser.firstName :
-        < Redirect to='/'/>}
+        { this.props.currentUser ?
+          <div className="user-info">
+          <div className="ui centered card">
+            <div className="image">
+            {this.props.currentUser.avatar !== null ? <img src={this.props.currentUser.avatar} /> : <img src="https://avatars3.githubusercontent.com/u/40308424?s=400&v=4" />}
+            </div>
+          </div>
+          </div>
+          : null }
+
       </div>
     )
   }
 }
+  // <img alt="user avatar" src={this.props.currentUser.avatar}/>
+      // : < Redirect to='/'/> }
--- src/components/UserTrails.js
+++ src/components/UserTrails.js
@@ -7,25 +7,32 @@ state = {
 }
 
 
-getUserHikes = () => {
-  if(this.props.currentUser){
-    let userId = this.props.currentUser.id
-      fetch(`http://localhost:3000/users/${userId}`)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          userTrails: data.hiking_lists
-        })
-      }, console.log("fetched"))
-  } else {
-    console.log("loading")
-  }
+// getUserHikes = () => {
+//   if(this.props.currentUser !== null){
+//     let userId = this.props.currentUser.id
+//       fetch(`http://localhost:3000/users/${userId}`)
+//       .then(res => res.json())
+//       .then(data => {
+//         this.setState({
+//           userTrails: data.hiking_lists
+//         })
+//       }, console.log("fetched"))
+//   } else {
+//     console.log("loading")
+//   }
+// }
+componentDidMount(){
+  this.props.currentUser ? console.log(this.props.currentUser.id) :
+  console.log("null")
+  // fetch(`http://localhost:3000/users`)
+  //   .then(res => res.json())
+  //   .then(data => console.log(data[0]))
 }
 
 render(){
     return(
       <div>
-
+    
       </div>
     )
   }
